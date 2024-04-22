051a7dbc05de3cb562d7fffe54774566e8b8fe3a
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-04-22 20:17:37 +0900

    0422

---
+++ js/var,let,const/script.js
@@ -0,0 +1,12 @@
+// var greeting = "hello";
+// console.log(greeting);
+// var greeting = "Hi";
+// console.log(greeting);
+// var greeting = "how are you?";
+// console.log(greeting);
+// let greeting = "hello";
+// console.log(greeting);
+// greeting = "hi";
+// console.log(greeting);
+const greeting = "hello";
+console.log(greeting);
