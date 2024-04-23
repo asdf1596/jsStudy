ef04bf76d1bc444f0804ed75b40c9eeb4be2e841
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-04-23 21:16:25 +0900

    0423

--- js/var,let,const/script.js
+++ js/var,let,const/script.js
@@ -8,5 +8,22 @@
 // console.log(greeting);
 // greeting = "hi";
 // console.log(greeting);
-const greeting = "hello";
-console.log(greeting);
+// const greeting = "hello";
+// console.log(greeting);
+// function func() {
+//     if(true){
+//         var a = 'a';
+//         console,log(a);
+//     }
+//     console.log(a)
+// }
+// console.log(a)
+// func()
+function func() {
+    if (true) {
+        const a = "a";
+        console, log(a);
+    }
+    console.log(a);
+}
+func();
